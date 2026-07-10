6b94dab924583fdd2cb28ae667e3f0b70016e65e
bricdocs <bricdocs.web@gmail.com>
2026-07-11 02:33:23 +0300

    Implement frame capture and preprocessing
    Added frame capture and preprocessing functionality.

--- js/app.js
+++ js/app.js
@@ -6,4 +6,14 @@ window.onload = async function () {
 
     console.log("Sistem hazır.");
 
+    const src = captureFrame(getVideo());
+
+    const gray = preprocess(src);
+
+    console.log("Frame:", src.cols, src.rows);
+    console.log("Gray :", gray.cols, gray.rows);
+
+    src.delete();
+    gray.delete();
+
 };
