8c4dee13d0480bc03c29b54a51df9c252db4788f
bricdocs <bricdocs.web@gmail.com>
2026-07-13 18:45:20 +0300

    Add templateBuilder.js with initial setup

---
+++ js/templateBuilder.js
@@ -0,0 +1,14 @@
+/*=========================================*
+ * templateBuilder.js
+ * Version 1.0
+ *=========================================*/
+
+console.log("templateBuilder.js hazır.");
+
+window.onload = async function () {
+
+    await waitForOpenCV();
+
+    console.log("Template Builder hazır.");
+
+};
