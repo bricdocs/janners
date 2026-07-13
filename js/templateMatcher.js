43cfba6ba6efa85ca427eb9cc6028fd65e57d695
bricdocs <bricdocs.web@gmail.com>
2026-07-13 18:08:58 +0300

    Add templateMatcher.js with initial template matching functions

---
+++ js/templateMatcher.js
@@ -0,0 +1,32 @@
+/*=========================================*
+ templateMatcher.js
+ Version 1.0
+*=========================================*/
+
+const TemplateMatcher = {
+
+    rankTemplates: [],
+    suitTemplates: []
+
+};
+
+//----------------------------------
+
+function loadTemplates()
+{
+    console.log("Template matcher hazır.");
+}
+
+//----------------------------------
+
+function matchRank(rankImage)
+{
+    return "?";
+}
+
+//----------------------------------
+
+function matchSuit(suitImage)
+{
+    return "?";
+}
