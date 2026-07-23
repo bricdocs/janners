eb21d7123ddd0b05837bd75545ef2a63a0016a5e
bricdocs <bricdocs.web@gmail.com>
2026-07-23 21:55:11 +0300

    Refactor Templates to use objects for ranks and suits

--- js/templateMatcher.js
+++ js/templateMatcher.js
@@ -4,18 +4,62 @@
  *=========================================*/
 
 const Templates = {
-    ranks: [],
-    suits: []
+
+    ranks: {},
+
+    suits: {}
+
 };
 
 async function loadTemplates() {
 
     console.log("Templates yükleniyor...");
 
-    // Şimdilik boş.
-    // Bir sonraki adımda resimleri burada yükleyeceğiz.
+    Templates.ranks["AH"] =
+        await loadImageMat("templates/ranks/AH.png");
+
+    Templates.suits["H"] =
+        await loadImageMat("templates/suits/H.png");
+
+    console.log(
+        "Rank templates:",
+        Object.keys(Templates.ranks)
+    );
+
+    console.log(
+        "Suit templates:",
+        Object.keys(Templates.suits)
+    );
 
     console.log("Templates hazır.");
 }
 
 console.log("templateMatcher.js hazır.");
+
+async function loadImageMat(path)
+{
+    return new Promise(resolve=>{
+
+        const img=new Image();
+
+        img.onload=function(){
+
+            const canvas=document.createElement("canvas");
+
+            canvas.width=img.width;
+            canvas.height=img.height;
+
+            const ctx=canvas.getContext("2d");
+
+            ctx.drawImage(img,0,0);
+
+            const mat=cv.imread(canvas);
+
+            resolve(mat);
+
+        };
+
+        img.src=path;
+
+    });
+}
