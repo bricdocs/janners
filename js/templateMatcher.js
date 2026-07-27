6fef440d2f43c547c160b555ebd03a48806a700e
bricdocs <bricdocs.web@gmail.com>
2026-07-27 14:38:29 +0300

    Remove debug saveMat calls from templateMatcher.js
    Removed debug code for saving runtime and template matrices.

--- js/templateMatcher.js
+++ js/templateMatcher.js
@@ -224,12 +224,6 @@ function matchTemplate(img, templates)
             img.copyTo(input);
         }
 
-    // DEBUG: Gerçek runtime Mat
-    //saveMat(input, "runtime_match.png");
-
-    // DEBUG: Gerçek template Mat
-    //saveMat(tpl, "template_match.png");        
-        
         const result = new cv.Mat();
 
 console.log(
